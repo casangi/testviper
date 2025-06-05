87274e058fc5519801f42cdc220a81e7a8e76800
Sandra María Castro <scastro@eso.org>
2025-06-05 18:11:26 +0200

    Directories for tests, conftest.py and integration sub-directory containing one example test for basic xradio integration.

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from toolviper.utils.data import download
+from xradio.measurement_set import convert_msv2_to_processing_set
+
+
+@pytest.fixture(scope="module")
+def sample_fixture():
+    return "sample_data"
+
