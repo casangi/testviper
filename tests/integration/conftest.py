d7e52bb7d1d1f96b2d1b0306cce7e16cad01f2c8
AWells <awells@nrao.edu>
2025-09-04 17:12:53 -0400

    Update to save database only

--- tests/integration/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from toolviper.utils.data import download
-from xradio.measurement_set import convert_msv2_to_processing_set
-
-
-@pytest.fixture(scope="module")
-def sample_fixture():
-    return "sample_data"
-
