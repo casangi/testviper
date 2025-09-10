89174f2aa3095f1dec5731321636d5fc09b2fc76
awells-nrao <awells-nrao@users.noreply.github.com>
2025-09-10 18:43:01 +0000

    Updated test db

---
+++ database/graphviper.sql
@@ -0,0 +1,58 @@
+-- CREATE TABLE statements
+
+-- Test Suites Table
+CREATE TABLE IF NOT EXISTS test_suites (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    component VARCHAR(255) NOT NULL,
+    branch VARCHAR(255) NOT NULL,
+    tests INTEGER DEFAULT 0,
+    failures INTEGER DEFAULT 0,
+    errors INTEGER DEFAULT 0,
+    skipped INTEGER DEFAULT 0,
+    time DECIMAL(10, 6) DEFAULT 0.0,
+    timestamp VARCHAR(50),
+    hostname VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Test Cases Table
+CREATE TABLE IF NOT EXISTS test_cases (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    suite_id INTEGER NOT NULL,
+    classname VARCHAR(500),
+    name VARCHAR(500) NOT NULL,
+    time DECIMAL(10, 6) DEFAULT 0.0,
+    file VARCHAR(500),
+    line VARCHAR(20),
+    status TEXT CHECK( status IN ('PASSED', 'FAILED', 'ERROR', 'SKIPPED')) NOT NULL,
+    failure_message TEXT,
+    failure_type VARCHAR(255),
+    failure_text TEXT,
+    error_message TEXT,
+    error_type VARCHAR(255),
+    error_text TEXT,
+    skip_message TEXT,
+    skip_reason TEXT,
+    system_out TEXT,
+    system_err TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (suite_id) REFERENCES test_suites(id)
+);
+
+
+-- INSERT statements
+INSERT OR IGNORE INTO test_suites (id, name, component, branch, tests, failures, errors, skipped, time, timestamp, hostname) 
+VALUES (337849276574739511, 'pytest', 'graphviper', '23-grafana-dashboard', 2, 
+        0, 0, 0, 14.275, 
+        '2025-09-10T18:42:43.162185+00:00', 'runnervmf4ws1');
+
+INSERT OR IGNORE INTO test_cases (id, suite_id, classname, name, time, file, line, status, 
+                     failure_message, failure_type, failure_text, error_message, error_type, 
+                     error_text, skip_message, skip_reason, system_out, system_err) 
+VALUES (337849276574739512, 337849276574739511, 'tests.test_graph_tools', 'test_map_reduce', 12.625, '', '', 'PASSED', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT OR IGNORE INTO test_cases (id, suite_id, classname, name, time, file, line, status, 
+                     failure_message, failure_type, failure_text, error_message, error_type, 
+                     error_text, skip_message, skip_reason, system_out, system_err) 
+VALUES (337849276574739513, 337849276574739511, 'tests.test_graph_tools', 'test_ps_partition', 1.625, '', '', 'PASSED', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
